42952e789f1080145e637a5be0151841209710ed
Tom Theisen <github@tomtheisen.com>
2023-07-23 11:06:11 -0700

    warn on zero deps detected

--- mutraction-react/src/syncFromTracker.ts
+++ mutraction-react/src/syncFromTracker.ts
@@ -1,11 +1,16 @@
 import { track, Tracker } from 'mutraction';
-import { useSyncExternalStore, memo } from 'react';  
+import { useSyncExternalStore } from 'react';  
 
 export function syncFromTracker<P extends {}>(tracker: Tracker, Component: React.FC<P>) {
     const TrackedComponent: React.FC<P> = function TrackedComponent(props: P, context?: any){
         const deps = tracker.startDependencyTrack();
         const component = Component(props, context);
         deps.endDependencyTrack();
+        if (deps.trackedObjects.size === 0) {
+            console.warn(
+                `No dependencies detected in ${Component.displayName ?? Component.name}. `
+                + `Ensure the component reads a tracked property to enable model synchronization.`);
+        }
 
         function subscribe(callback: () => void) {
             const subscription = tracker.subscribe(callback);
