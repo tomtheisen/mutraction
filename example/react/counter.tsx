6dc06492c26413d3fe9d0ccb48f11eff79ccb736
Tom Theisen <github@tomtheisen.com>
2023-07-24 19:51:24 -0700

    exclude tests from publish

--- example/react/counter.tsx
+++ example/react/counter.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { createRoot } from 'react-dom/client';
 import { trackAndSync } from 'mutraction-react';
 
-const [model, sync] = trackAndSync({count: 0});
+const [model, sync] = trackAndSync({ count: 0 });
 
 function increase() { ++model.count; }
 
-const CountDisplay = sync(() => <p>Click count: {model.count}</p>);
-const ClickButton = sync(() => <button onClick={increase}>+1</button>);
+const CountDisplay = sync(() => <p>Click count: { model.count }</p>);
+const ClickButton = sync(() => <button onClick={ increase }>+1</button>);
 
 const App = sync(function App() {
     return <>
