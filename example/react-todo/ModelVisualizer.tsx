6f5d099885f08f90b69a8e94c4f61b8cd03c8e95
Tom Theisen <github@tomtheisen.com>
2023-08-08 00:20:07 -0700

    better viz

---
+++ example/react-todo/ModelVisualizer.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+
+export function ModelVisualizer({ value }: { value: unknown; }) {
+    if (Array.isArray(value)) {
+        return <ol>{value.map((e, i) => 
+            <li key={i}>
+                <ModelVisualizer value={e} />
+            </li>)}</ol>;
+    }
+    else if (typeof value === "object" && value) {
+        return <ul>{
+            Object.entries(value).map(([key, val]) => 
+                <li key={key}>
+                    {key}: <ModelVisualizer value={val} />
+                </li>)
+        }</ul>;
+    }
+    else if (typeof value === "string") {
+        return JSON.stringify(value);
+    }
+    else {
+        return String(value);
+    }
+}
