3c93ec944d7d200e4c899daad431e2b994e8fe11
Tom Theisen <github@tomtheisen.com>
2023-08-08 07:36:16 -0700

    highlight node on change in viz

--- example/react-todo/ModelVisualizer.tsx
+++ example/react-todo/ModelVisualizer.tsx
@@ -1,17 +1,26 @@
 import * as React from "react";
 
 export function ModelVisualizer({ value }: { value: unknown; }) {
+    const ref = React.useRef<HTMLElement>(null);
+
+    React.useEffect(() => {
+        if (!ref.current) return;
+        const el = ref.current;
+        el.style.backgroundColor = "aqua";
+        setTimeout(() => el.style.backgroundColor = "", 200);
+    });
+
     if (Array.isArray(value)) {
-        return <ol>{value.map((e, i) => 
-            <li key={i}>
-                <ModelVisualizer value={e} />
+        return <ol ref={ ref as any }>{value.map((e, i) => 
+            <li key={ i }>
+                <ModelVisualizer value={ e } />
             </li>)}</ol>;
     }
     else if (typeof value === "object" && value) {
-        return <ul>{
+        return <ul ref={ ref as any }>{
             Object.entries(value).map(([key, val]) => 
-                <li key={key}>
-                    {key}: <ModelVisualizer value={val} />
+                <li key={ key }>
+                    { key }: <ModelVisualizer value={ val } />
                 </li>)
         }</ul>;
     }
@@ -19,6 +28,6 @@ export function ModelVisualizer({ value }: { value: unknown; }) {
         return JSON.stringify(value);
     }
     else {
-        return String(value);
+        return <span ref={ ref as any }>{ String(value) }</span>;
     }
 }
