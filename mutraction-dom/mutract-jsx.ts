e6da5320df2b6ee30c4a70c9dd3fa300b6a73033
Tom Theisen <github@tomtheisen.com>
2023-08-12 10:18:22 -0700

    baseline expression level AST transformers

--- mutraction-dom/mutract-jsx.ts
+++ mutraction-dom/mutract-jsx.ts
@@ -5,8 +5,6 @@ import type { PluginObj } from '@babel/core';
 export type Babel = typeof BabelCoreNamespace;
 export type BabelTypes = typeof BT;
 
-const Visited = Symbol("Visited");
-
 const t = BabelCoreNamespace.types;
 function jsxChild(child: 
     BT.JSXText | 
@@ -42,54 +40,88 @@ function jsxChild(child:
     throw Error("Unsupported child type " + type);
 }
 
-function toReference(name: BT.JSXIdentifier | BT.JSXMemberExpression): BT.Identifier | BT.MemberExpression {
+function jsxId2Id(name: BT.JSXIdentifier | BT.JSXMemberExpression): BT.Identifier | BT.MemberExpression {
     if (name.type === "JSXIdentifier") return t.identifier(name.name);
-    return t.memberExpression(toReference(name.object), t.identifier(name.property.name));
+    return t.memberExpression(jsxId2Id(name.object), t.identifier(name.property.name));
 }
 
-export default function({ types: t }: Babel): PluginObj {
-    return {
-        visitor: {
-            // BinaryExpression(path) {
-            //     if (path.node.operator !== "===") return;
-            //     path.node.left = t.identifier("sebmck");
-            //     path.node.right = t.identifier("dork");
-            // },
-            JSXElement(path) {
-                let name = path.node.openingElement.name;
-                let id = "";
+function jsxProp(attr: BT.JSXAttribute | BT.JSXSpreadAttribute) : BT.ObjectProperty | BT.SpreadElement {
+    const type = attr.type;
+    if (type === 'JSXAttribute') {
+        if (attr.name.type === 'JSXNamespacedName') 
+            throw Error("Unsupported namespace in JSX attribute");
 
-                if (name.type === "JSXIdentifier" && /^[a-z]/.test(name.name)) {
-                    // is DOM (host) element
+        return t.objectProperty(
+            t.identifier(attr.name.name),
+            jsxAttrVal2Prop(attr.value));
+    }
+    else if (type === 'JSXSpreadAttribute') {
+        return t.spreadElement(attr.argument);
+    }
+    throw Error('Unsupported attribute type: ' + type);
+}
 
-                    const jsxChildren: BT.Expression[] = [];
-                    for (const child of path.node.children) {
-                        const compiled = jsxChild(child);
-                        if (compiled) jsxChildren.push(compiled);
-                    }
+function jsxAttrVal2Prop(attrVal: 
+    BT.JSXExpressionContainer |
+    BT.JSXElement |
+    BT.JSXFragment |
+    BT.StringLiteral |
+    null | undefined
+): BT.Expression {
+    // bare attribute e.b. <input disabled />
+    if (!attrVal) return t.booleanLiteral(true);
 
-                    path.replaceWith(t.callExpression(
-                        t.identifier("element"), 
-                        [
-                            t.stringLiteral(name.name),
-                            t.objectExpression([]), // TODO props
-                            ...jsxChildren
-                        ]
-                    ));
-                    return;
+    const type = attrVal.type;
+    if (type === 'StringLiteral') {
+        return t.stringLiteral(attrVal.value);
+    }
+    else if (type === 'JSXExpressionContainer') {
+        if (attrVal.expression.type === 'JSXEmptyExpression') return t.booleanLiteral(true);
+        return t.arrowFunctionExpression([], attrVal.expression)
+    }
+    else {
+        return attrVal; // process later / TODO?
+    }
+}
+
+const mutractPlugin: PluginObj = {
+    visitor: {
+        JSXElement(path) {
+            let name = path.node.openingElement.name;
+
+            if (name.type === "JSXNamespacedName")
+                throw Error("This JSX element type is not supported");
+
+            const props = path.node.openingElement.attributes.map(jsxProp);
+
+            if (name.type === "JSXIdentifier" && /^[a-z]/.test(name.name)) {
+                // treat as DOM element
+                const jsxChildren: BT.Expression[] = [];
+                for (const child of path.node.children) {
+                    const compiled = jsxChild(child);
+                    if (compiled) jsxChildren.push(compiled);
                 }
 
-                if (name.type === "JSXNamespacedName")
-                    throw Error("This JSX element type is not supported");
-
-                // it's a component, these things practically render themselves
-                return t.callExpression(
-                    toReference(name),
+                path.replaceWith(t.callExpression(
+                    t.identifier("element"), 
                     [
-                        t.objectExpression([]), // TODO props
+                        t.stringLiteral(name.name),
+                        t.objectExpression(props),
+                        ...jsxChildren
                     ]
-                );
+                ));
+                return;
             }
+
+            // it's a component, these things practically render themselves
+            path.replaceWith(t.callExpression(
+                jsxId2Id(name),
+                [ t.objectExpression(props) ]
+            ));
         }
-    };
+    }
+};
+
+export default function(_: Babel): PluginObj {
+    return mutractPlugin;
 }
