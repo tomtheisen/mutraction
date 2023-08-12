e5812222d3d11d41ab2f5d3dade86294d944f864
Tom Theisen <github@tomtheisen.com>
2023-08-12 02:18:05 -0700

    binding concept looks good. still need mounting and compilation

--- mutraction-dom/dist/index.js
+++ mutraction-dom/dist/index.js
@@ -528,13 +528,28 @@
   }
 
   // out/index.jsx
+  var suppress = { suppressUntrackedWarning: true };
   function element(name, attrGetters, ...children) {
     const el = document.createElement(name);
     let blank = void 0;
     for (let [name2, attrGetter] of Object.entries(attrGetters ?? {})) {
       if (name2 === "if") {
+        effect(tracker, () => {
+          if (attrGetter())
+            blank?.replaceWith(el);
+          else
+            el.replaceWith(blank ??= document.createTextNode(""));
+        }, suppress);
+      } else if (name2 === "style") {
+        effect(tracker, () => Object.assign(el.style, attrGetter()), suppress);
+      } else if (name2 === "classList") {
+        effect(tracker, () => {
+          const classMap = attrGetter();
+          for (const e of Object.entries(classMap))
+            el.classList.toggle(...e);
+        }, suppress);
       } else {
-        effect(tracker, () => el[name2] = attrGetter());
+        effect(tracker, () => el[name2] = attrGetter(), suppress);
       }
     }
     el.append(...children);
@@ -544,16 +559,18 @@
     const result = getter();
     if (result instanceof HTMLElement)
       return result;
+    if (result instanceof Text)
+      return result;
     let node = document.createTextNode("");
     effect(tracker, () => {
       const newNode = document.createTextNode(String(getter() ?? ""));
       node.replaceWith(newNode);
       node = newNode;
-    });
+    }, suppress);
     return node;
   }
   var [model, tracker] = track({ val: "hello", tab: 5, show: false });
-  var d = element("div", { tabIndex: () => model.tab }, child(() => model.val), child(() => element("p", { if: () => model.show }, "lorem")), child(() => element("p", { if: () => false }, "never")));
+  var d = element("div", { tabIndex: () => model.tab }, child(() => model.val), child(() => element("p", {}, "just something")), child(() => element("p", { classList: () => ({ stinky: model.show }) }, "touch of class")), element("p", { style: () => ({ outline: "green solid 2px" }) }, "just something unwrapped once"), child(() => element("p", { if: () => model.show }, "intermittently")), child(() => element("p", { if: () => false }, "never")), "");
   document.getElementById("root")?.appendChild(d);
   var x = 0;
   setInterval(() => {
--- mutraction-dom/index.tsx
+++ mutraction-dom/index.tsx
@@ -1,10 +1,18 @@
 import { track, effect, Tracker } from 'mutraction';
 
+type AttributeType<E extends keyof HTMLElementTagNameMap, K extends keyof HTMLElementTagNameMap[E]> = 
+    K extends "style" ? Partial<CSSStyleDeclaration> :
+    K extends "classList" ? Record<string, boolean> :
+    HTMLElementTagNameMap[E][K];
+
 type ElementProps<E extends keyof HTMLElementTagNameMap> = {
-    [K in keyof HTMLElementTagNameMap[E]]?: () => HTMLElementTagNameMap[E][K];
+    [K in keyof HTMLElementTagNameMap[E]]?:
+        () => AttributeType<E, K>;
 } & {
     if?: () => boolean;
 }; 
+
+const suppress = { suppressUntrackedWarning: true } as const;
 
 // function element(name: string, attrs: Record<string, () => string|number|boolean>, ...children: ChildNode[]): HTMLElement;
 function element<E extends keyof HTMLElementTagNameMap>(
@@ -16,32 +24,38 @@ function element<E extends keyof HTMLElementTagNameMap>(
     let blank: Text | undefined = undefined;
     for (let [name, attrGetter] of Object.entries(attrGetters ?? {})) {
         if (name === "if") {
-            // effect(tracker, () => {
-            //     if (attrGetter()) {
-            //         blank?.replaceWith(el);
-            //     }
-            //     else {
-            //         el.replaceWith(blank ??= document.createTextNode(""));
-            //     }
-            // });
+            effect(tracker, () => {
+                if (attrGetter()) blank?.replaceWith(el);
+                else el.replaceWith(blank ??= document.createTextNode(""));
+            }, suppress);
+        }
+        else if (name === "style") {
+            effect(tracker, () => Object.assign(el.style, attrGetter()), suppress);
+        }
+        else if (name === "classList") {
+            effect(tracker, () => {
+                const classMap = attrGetter() as Record<string, boolean>;
+                for (const e of Object.entries(classMap)) el.classList.toggle(...e);
+            }, suppress);
         }
         else {
-            effect(tracker, () => (el as any)[name] = attrGetter());
+            effect(tracker, () => (el as any)[name] = attrGetter(), suppress);
         }
     }
     el.append(...children);
     return blank ?? el;
 }
 
-function child(getter: () => number | string | bigint | null | undefined | HTMLElement): ChildNode {
+function child(getter: () => number | string | bigint | null | undefined | HTMLElement | Text): ChildNode {
     const result = getter();
     if (result instanceof HTMLElement) return result;
+    if (result instanceof Text) return result;
     let node = document.createTextNode("");
     effect(tracker, () => {
         const newNode = document.createTextNode(String(getter() ?? ""));
         node.replaceWith(newNode);
         node = newNode;
-    });
+    }, suppress);
     return node;
 }
 
@@ -49,8 +63,12 @@ const [model, tracker] = track({ val: "hello", tab: 5, show: false });
 
 const d = element("div", { tabIndex: () => model.tab },
     child(() => model.val),
-    child(() => element("p", { if: () => model.show }, "lorem")),
-    child(() => element("p", { if: () => false }, "never"))
+    child(() => element("p", {}, "just something")),
+    child(() => element("p", { classList: () => ({ stinky: model.show }) }, "touch of class")),
+    element("p", { style: () => ({ outline: "green solid 2px" }) }, "just something unwrapped once"),
+    child(() => element("p", { if: () => model.show }, "intermittently")),
+    child(() => element("p", { if: () => false }, "never")),
+    ""
 );
 
 /*
