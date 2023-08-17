5a05bad66976496770718b06a42e0d4aee3c4e65
Tom Theisen <github@tomtheisen.com>
2023-08-16 22:19:50 -0700

    partial object for inline style prop

--- mutraction-dom/src/jsx.d.ts
+++ mutraction-dom/src/jsx.d.ts
@@ -1,12 +1,17 @@
 import type { Tracker } from "mutraction";
 
+type PartialIfObject<T> = T extends undefined | string | number | boolean | bigint | symbol | any[] | Function
+    ? T
+    : Partial<T>;
+
 export namespace JSX {
     export type Element = Node;
 
     export type ElementAttributesProperty = never;
 
     export type MutractionElement<ElementType extends keyof HTMLElementTagNameMap> = {
-        [PropType in keyof HTMLElementTagNameMap[ElementType]]?: HTMLElementTagNameMap[ElementType][PropType];
+        [PropType in keyof HTMLElementTagNameMap[ElementType]]?: 
+            PartialIfObject<HTMLElementTagNameMap[ElementType][PropType]>;
     }
     & {
         "mu:if"?: boolean;
