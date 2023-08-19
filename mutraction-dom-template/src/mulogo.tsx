b83b9a476378f55b7fd3332e622cf491f8171905
Tom Theisen <github@tomtheisen.com>
2023-08-19 15:24:25 -0700

    bike shedding on the logo

--- mutraction-dom-template/src/mulogo.tsx
+++ mutraction-dom-template/src/mulogo.tsx
@@ -1,100 +1,44 @@
+const treads = 32;
+let stops = "";
+
+for (let i = 0; i < treads; i++) {
+  const color = i % 2 ? "#0000" : "#000";
+  stops += `\n , ${ color } ${ i / treads }turn, ${ color } ${ (i+1) / treads }turn`;
+}
+
 const styles = `
-* {
-    box-sizing: border-box;
-  }
-  
-  body {
-    background: #333;
-  }
-  
   .tread {
-    position: relative;
-    padding:10px;
-    width: 200px;
-    height: 200px;
-    border-radius: 200px;
-    background: conic-gradient(from 90deg at 50% 50%
-      , #000  0turn, #000  0.03125turn
-      , #0000 0.03125turn, #0000 0.0625turn
-      , #000  0.0625turn, #000  0.09375turn
-      , #0000 0.09375turn, #0000 0.125turn
-      , #000  0.125turn, #000  0.15625turn
-      , #0000 0.15625turn, #0000 0.1875turn
-      , #000  0.1875turn, #000  0.21875turn
-      , #0000 0.21875turn, #0000 0.25turn
-      , #000  0.25turn, #000  0.28125turn
-      , #0000 0.28125turn, #0000 0.3125turn
-      , #000  0.3125turn, #000  0.34375turn
-      , #0000 0.34375turn, #0000 0.375turn
-      , #000  0.375turn, #000  0.40625turn
-      , #0000 0.40625turn, #0000 0.4375turn
-      , #000  0.4375turn, #000  0.46875turn
-      , #0000 0.46875turn, #0000 0.5turn
-      , #000  0.5turn, #000  0.53125turn
-      , #0000 0.53125turn, #0000 0.5625turn
-      , #000  0.5625turn, #000  0.59375turn
-      , #0000 0.59375turn, #0000 0.625turn
-      , #000  0.625turn, #000  0.65625turn
-      , #0000 0.65625turn, #0000 0.6875turn
-      , #000  0.6875turn, #000  0.71875turn
-      , #0000 0.71875turn, #0000 0.75turn
-      , #000  0.75turn, #000  0.78125turn
-      , #0000 0.78125turn, #0000 0.8125turn
-      , #000  0.8125turn, #000  0.84375turn
-      , #0000 0.84375turn, #0000 0.875turn
-      , #000  0.875turn, #000  0.90625turn
-      , #0000 0.90625turn, #0000 0.9375turn
-      , #000  0.9375turn, #000  0.96875turn
-      , #0000 0.96875turn, #0000 1turn
+    display: inline-block;
+    width:200px;
+    height:200px;
+    border-radius:100%;
+    background: 
+      radial-gradient(#fff 40%, #000 41%, #000 63%, #0000 64%),
+      conic-gradient(from 90deg at 50% 50%
+      ${ stops }
     );
   }
-  
-  .sidewall {
-    background: #000;
-    width: 100%;
-    height: 100%;
-    border-radius: 100%;
-    padding: 35px;
-  }
-  
-  .inner {
-    background: #fff;
-    width: 100%;
-    height: 100%;
-    border-radius: 100%;
-  }
-  
+
   .mu {
+    width: 100%;
+    height: 100%;
+    padding: 83px 52px;
     color: #0256ff;
-    font-size: 110pt;
-    font-style: italic;
-    font-weight: bold;
-    font-family: "Calibri", "Arial", "Helvetica", sans-serif;
+    font: italic bold 1000% "Calibri", "Arial", "Helvetica", sans-serif;
     line-height: 0;
-    position: relative;
-    top: 40px;
-    left: 14px;
   }
-  
+
   @keyframes rotating {
     from { transform: rotate(0deg); }
     to { transform: rotate(360deg); }
   }
-  .rotating {
-    animation: rotating 20s linear infinite;
-  }
-  .rotating-reverse {
-    animation: rotating 20s linear infinite reverse;
-  }
-  `;
+  .rotating         { animation: rotating 20s linear infinite; }
+  .rotating-reverse { animation: rotating 20s linear infinite reverse; }
+`;
 
 export const mu = <>
     <div className="tread rotating">
-        <div className="sidewall rotating-reverse">
-            <div className="inner">
-            <div className="mu">μ</div>
-            </div>
-        </div>
+      <div className="mu rotating-reverse">μ</div>
     </div>
     <style>
         {styles}
