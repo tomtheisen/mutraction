0627a43d3d2716db389656b8b904d598d5477bd0
Tom Theisen <github@tomtheisen.com>
2023-08-21 21:34:37 -0700

    lazy recipe

--- mutraction-dom-docs/src/routes.tsx
+++ mutraction-dom-docs/src/routes.tsx
@@ -23,6 +23,7 @@ import { trackerDoc } from "./trackerDoc.js";
 import { mounting } from "./mounting.js";
 import { radio } from "./radio.js";
 import { spinner } from "./spinner.js";
+import { lazy } from "./lazy.js";
 
 export const routes = Router(
     { pattern: '#start', element: getStarted },
@@ -47,7 +48,7 @@ export const routes = Router(
     { pattern: '#recipes/mounting', element: mounting },
     { pattern: '#recipes/radio', element: radio },
     { pattern: '#recipes/spinner', element: spinner },
-    { pattern: '#recipes/lazy', element: <span>coming soon</span> },
+    { pattern: '#recipes/lazy', element: lazy },
     { pattern: '#recipes/array', element: <span>coming soon</span> },
 
     { pattern: '#why', element: why },
