6e173ea188caecd37ea8d42a2f7a6acaa2e1f1ec
Tom Theisen <github@tomtheisen.com>
2023-08-24 23:23:30 -0700

    sandbox

--- mutraction-sandbox/src/index.tsx
+++ mutraction-sandbox/src/index.tsx
@@ -2,6 +2,8 @@ import compileJsx from "mutraction-dom/compile-jsx";
 import syntaxJsx from "@babel/plugin-syntax-jsx";
 import { transform } from "@babel/standalone";
 
+const storageKey = "mu_playground_source";
+
 export function muCompile(source: string) {
     const options = { plugins: [syntaxJsx, compileJsx] };
     const { code } = transform(source, options);
@@ -12,8 +14,19 @@ const sourceBox = document.getElementById("source") as HTMLInputElement;
 const runButton = document.getElementById("run")!;
 const frame = document.getElementById("frame") as HTMLIFrameElement;
 
-runButton.addEventListener("click", () => {
+function run() {
     const code = sourceBox.value;
+    sessionStorage.setItem(storageKey, code);
     const compiled = muCompile(code);
-    frame.contentWindow?.postMessage(compiled, "*");
+    frame.contentWindow?.location.reload();
+    frame.addEventListener("load", ev => {
+        frame.contentWindow?.postMessage(compiled, "*");
+    }, { once: true });
+}
+
+runButton.addEventListener("click", run);
+window.addEventListener("keydown", ev => {
+    if (ev.key === "Enter" && ev.ctrlKey) run();
 });
+
+if (sourceBox.value) run();
