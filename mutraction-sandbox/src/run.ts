a0e074db3ffb7659d20d3c0c095fbeabca909b55
Tom Theisen <github@tomtheisen.com>
2023-11-16 23:53:01 -0800

    run script was pulling in whole lib

--- mutraction-sandbox/src/run.ts
+++ mutraction-sandbox/src/run.ts
@@ -3,7 +3,10 @@ import { compress, decompress } from "./compress.js";
 async function init() {
     if (location.hash.length <= 1) location.href = "./";
     const { src, js } = JSON.parse(await decompress(location.hash.substring(1)));
-    document.body.append(<script type="module" textContent={ js } />);
+    const module = document.createElement("script");
+    module.type = "module";
+    module.textContent = js;
+    document.body.append(module);
 
     const compressedSrc = await compress(src);
     const link = (document.getElementById("mu-sandbox-link-template") as HTMLTemplateElement).content;
