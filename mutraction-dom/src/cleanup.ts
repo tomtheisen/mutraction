f23461b3449cc676dac6ec4b86f3aca1a8338b07
Tom Theisen <github@tomtheisen.com>
2023-12-23 15:12:46 -0800

    defer cleanup with requestIdleCallback

--- mutraction-dom/src/cleanup.ts
+++ mutraction-dom/src/cleanup.ts
@@ -12,11 +12,58 @@ export function registerCleanup(node: ChildNode, subscription: Subscription) {
     }
 }
 
-export function cleanup(node: ChildNode) {
+export const cleanup = (scheduleCleanup<ChildNode>).bind(null, doCleanup);
+
+function doCleanup(node: ChildNode) {
     const cleanups = nodeCleanups.get(node);
     cleanups?.forEach(s => s.dispose());
 
-    if (node instanceof Element) {
-        node.childNodes.forEach(cleanup);
+    if (node instanceof Element) node.childNodes.forEach(cleanup);
+}
+
+type CleanupItem<T = any> = {
+    task: (data: T) => void;
+    data: T;
+}
+
+let pending = false;
+let queue: CleanupItem[] = [];
+
+// dumb polyfill, come on safari
+if (!("requestIdleCallback" in globalThis)) {
+    const never: IdleDeadline = {
+        didTimeout: false,
+        timeRemaining() { return 1e3; },
+    }
+
+    function requestIdleCallback(callback: IdleRequestCallback) {
+        requestAnimationFrame(callback.bind(null, never));
+    }
+    Object.assign(globalThis, { requestIdleCallback });
+}
+
+function processQueue(deadline: IdleDeadline) {
+    let complete = 0;
+    let start = performance.now();
+
+    while(queue.length) {
+        const { task, data} = queue.shift()!;
+        task(data);
+
+        const elapsed = performance.now() - start;
+        const average = elapsed / ++complete;
+        if (average * 2 > deadline.timeRemaining()) break;
+    }
+
+    if (queue.length) requestIdleCallback(processQueue);
+    else pending = false;
+}
+
+export function scheduleCleanup<T>(task: (t: T) => void, data: T) {
+    queue.push({ task, data });
+
+    if (!pending) {
+        requestIdleCallback(processQueue);
+        pending = true;
     }
 }
