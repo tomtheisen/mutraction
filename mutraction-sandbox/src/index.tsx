e099b2937162e319185eaf10376b37cb8361d93a
Tom Theisen <github@tomtheisen.com>
2024-01-04 19:50:15 -0800

    reduction

--- mutraction-sandbox/src/index.tsx
+++ mutraction-sandbox/src/index.tsx
@@ -1,15 +1,5 @@
 import { muLogo } from "./mulogo.js";
-import { PromiseLoader, Swapper, effect, makeLocalStyle, track, version } from "mutraction-dom";
-import { compress, decompress } from "./compress.js";
-import { defaultSource } from "./defaultSource.js";
-import type * as monacoType from "monaco-editor";
-import { getJsxDts, getMutractionDom as getMutractionDomDts } from "./mutractionDomModuleTypeSource.js";
-import { getScaffoldZipUrl } from "./makeZip.js";
-import { getSelfContainedUrl } from "./selfContained.js";
-import { muCompile } from "./compile.js";
-import { getShortLink } from "./shortLinks.js";
-
-const storageKey = "mu_playground_source";
+import { PromiseLoader, Swapper, effect, track, version } from "mutraction-dom";
 
 const query = new URL(location.href).searchParams;
 const appState = track({
@@ -32,12 +22,12 @@ function notify(message: string) {
 }
 
 const runButton =
-    <button onclick={ () => run() }>
+    <button>
         Run ▶️ <small className="narrow-hide">(<kbd>ctrl + enter</kbd>)</small>
     </button>;
 
 const saveButton = 
-    <button onclick={ share }>
+    <button>
         Share <small className="narrow-hide">(<kbd>ctrl + S</kbd>)</small>
     </button>;
 
@@ -59,11 +49,6 @@ effect(() => {
 
 const frame = <iframe src="output.html" hidden={ appState.outpane !== "app" } /> as HTMLIFrameElement;
 const jsOutput = <output style={{ display: appState.outpane === "js" ? "block" : "none" }} textContent={ appState.compiledCode } /> as HTMLOutputElement;
-
-async function getRunLink(src: string) {
-    const js = await muCompile(src);
-    return "run.html#" + await compress(JSON.stringify({ src, js }));
-}
 
 function hamburger() {
     const hamburgerState = track({ 
@@ -146,56 +131,11 @@ function hamburger() {
     return hamburger;
 }
 
-function run(code: string | undefined = "") {
-    if (!code) return;
-
-    sessionStorage.setItem(storageKey, code);
-    try {
-        appState.compiledCode = muCompile(code);
-        frame.contentWindow?.location.reload();
-
-        // if you run, you want to see output
-        if (appState.view === "code") appState.view = "normal";
-    }
-    catch (err) {
-        const style: Partial<CSSStyleDeclaration> = {
-            whiteSpace: "pre",
-            fontFamily: "monospace",
-            color: "#ff9e9e",
-            padding: "1em",
-            position: "relative",
-            overflowX: "auto",            
-        };
-    }
-}
-
-window.addEventListener("message", ev => {
-    switch (ev.data.type) {
-        case "run": 
-            run();
-            break;
-
-        case "save":
-            share();
-            break;
-
-        case "zen":
-            document.firstElementChild?.classList.toggle("zen");
-            break;
-
-        case "display":
-            appState.view = ev.data.displayType;
-            break;
-    }
-});
-
 window.addEventListener("keydown", ev => {
     if (ev.key === "Enter" && ev.ctrlKey) {
-        run();
     }
     else if ((ev.key === "s" || ev.key ==="S") && ev.ctrlKey && !ev.shiftKey) {
         ev.preventDefault();
-        share();
     }
     else if (ev.key === "1" && ev.altKey) {
         appState.view = "code";
@@ -231,56 +171,6 @@ function updateSize(ev: MouseEvent) {
     ev.stopPropagation();
 }
 
-async function share() {
-    appState.shortLinkLoading = appState.shortLinkDialogOpen = true;
-    appState.shortRunLinkError = appState.shortLinkError = "";
-    appState.shortRunLink = appState.shortLink = "";
-}
-
-async function doCopy(val: string) {
-    try {
-        await navigator.clipboard.writeText(val);
-        notify("URL copied to clipboard");
-    }
-    catch (err) {
-        console.error(err);
-        notify("Failed to copy: " + String(err));
-    }
-}
-
-const dialog = 
-    <dialog>
-        <h4>Share Sandbox Link</h4>
-        <button className="close" onclick={ () => appState.shortLinkDialogOpen = false }>
-            &times;
-        </button>
-
-        <p mu:if={ !!appState.shortLink }>
-            <button onclick={ () => doCopy(appState.shortLink) }>Copy</button>
-            Sandbox: <a target="_blank" href={ appState.shortLink }>{ appState.shortLink }</a>
-        </p>
-        <p mu:else mu:if={ appState.shortLinkLoading }>Loading …</p>
-        <p mu:else mu:if={ !!appState.shortLinkError }>
-            { appState.shortLinkError }<br/>
-            You can still use the long link in the URL bar.
-        </p>
-
-        <p mu:if={ !!appState.shortRunLink }>
-            <button onclick={ () => doCopy(appState.shortRunLink) }>Copy</button>
-            Standalone: <a target="_blank" href={ appState.shortRunLink }>{ appState.shortRunLink }</a>
-        </p>
-        <p mu:else mu:if={ appState.shortRunLinkLoading }>Loading …</p>
-        <p mu:else mu:if={ !!appState.shortRunLinkError }>
-            { appState.shortRunLinkError }<br/>
-            You can still use the long link in the URL bar.
-        </p>
-    </dialog> as HTMLDialogElement;
-
-effect(() => {
-    if (appState.shortLinkDialogOpen) dialog.showModal();
-    else dialog.close();
-});
-
 const app =
     <>
         <header>
@@ -310,12 +200,4 @@ const app =
         </main>
     </>;
 
-document.body.append(app, dialog);
-
-async function init() {
-    const source = location.hash.length > 1
-        ? await decompress(location.hash.substring(1))
-        : sessionStorage.getItem(storageKey) ?? defaultSource;
-    run(source);
-}
-init();
+document.body.append(app);
